825c672289fb0438c09cdba7508ff191f0cded20
FBR251 <ancartem9@gmail.com>
2025-09-13 13:15:22 +0300

    3v

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 class Student:
-    print('Hi!')
-    def __init__(self):
-        self.height = 160
-        print("I am alive!")
+    amount_of_students = 0
+    def __init__(self, height=160,name=None):
+        self.height = height
+        Student.amount_of_students+=1
+        self.name=name
+    def grow(self, height=1):
+        self.height+=height
 
+    def __str__(self):
+        return f"I am a student. My name is {self.name}."
 
-first_student = Student()
+    def __del__(self):
+        print("Training is over. I am now an expert!")
 
+nick = Student(name='Nick')
+kate = Student(height=170)
+nick.grow(height=15)
+print(kate.height)
+print(nick.height)
+print(nick)
